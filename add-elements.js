6ca00356921470003fbc277537eefedbb65a5889
subelta <40162440+subelta@users.noreply.github.com>
2019-05-23 23:13:33 +0300

    accessibility, enter key, textfield resize

--- add-elements.js
+++ add-elements.js
@@ -1,6 +1,9 @@
 "use strict"
 
-document.addEventListener("click", function(e) {
+const MAXLEN_CARD_INPUT = 2000;
+const MAXLEN_HEADER_INPUT = 500;
+
+document.addEventListener("click", e => {
     let clicked = e.target;
     const buttons = {
         "add-card": turnOnCardInput, 
@@ -13,8 +16,42 @@ document.addEventListener("click", function(e) {
     
     for (let btn in buttons) {
         if(clicked.classList.contains(btn)) {
-            buttons[btn](clicked);
+            buttons[btn](clicked, e);
         }
+    }
+});
+
+
+//ресайз textfield'a
+document.addEventListener("input", e => { 
+    if (e.target.classList.contains("input-card")) {
+        let inp = e.target;
+        inp.style.height = "inherit";
+
+        inp.style.height = inp.scrollHeight + "px";
+    }
+});
+
+
+//Enter для полей ввода
+document.addEventListener("keydown", e => {
+    const inputsCl = ["input-card", "input-column"];
+    const buttonsCl = [".confirm-add-card", ".confirm-add-column"];
+
+
+    if (e.keyCode !== 13) { 
+        return;
+    }
+
+    e.preventDefault();
+
+    const t = e.target.classList;
+    if (t.contains(inputsCl[0]) || t.contains(inputsCl[1])) {
+        const btns = e.target
+                      .parentElement
+                      .querySelectorAll(`${ buttonsCl[0] }, ${ buttonsCl[1] }`);
+        // console.log(btns)
+        btns[0].click();
     }
 });
 
@@ -43,13 +80,14 @@ function turnOffCardInput(clicked) {
 }
 
 
-function createCard(clicked) {
+function createCard(clicked, e) {
     let footer = clicked.parentElement
                         .parentElement
                         .parentElement;
     let cardsList = footer.previousElementSibling;
     let inputCard = clicked.parentElement
                            .previousElementSibling;
+    e.preventDefault();
 
     const cardText = inputCard.value.trim();
     if (cardText) {
@@ -61,22 +99,29 @@ function createCard(clicked) {
         console.log("card created!");
     }
     inputCard.value = '';
+    inputCard.style.height = "inherit";
 }
 
 
 function addColumnInp(clicked) {
+    let id = generateId("inp-col-head-");
     const colMarkup = `
-        <section class="input-section">
+        <form class="input-section">
+            <label for=${id} class="sr-only"> <!--Доступность-->
+                Введите название колонки 
+            </label>
             <input 
+                id=${id}
                 class="input-column" 
                 type="text" 
                 name="column-name" 
-                placeholder="Введите название колонки">
+                placeholder="Введите название колонки"
+                maxlength="${MAXLEN_HEADER_INPUT}">
             <section class="controls">
                 <button class="confirm-add-column">Добавить колонку</button>
                 <span class="discard-add-column"></span>
             </section>
-        </section>
+        </form>
     `
     let column = document.createElement("article");
     column.classList.add("column");
@@ -97,34 +142,41 @@ function deleteColumnInp(clicked) {
 }
 
 
-function generateColumn(clicked) {
-    let inputSection = clicked.parentElement
-                              .parentElement;
+function generateColumn(clicked, e) {
+    e.preventDefault();
+    
+    let inputSection = clicked.parentElement.parentElement;
     let column = inputSection.parentElement;
+    let id = generateId("inp-card-");
+    const heading = inputSection.querySelector(".input-column").value;
 
-    const heading = inputSection.firstElementChild.value;
     if (!heading) {
         return;
     }
-    
     inputSection.remove();
+    
     const fullColMarkup = `
         <header>
             <h1 class="column-header">${ heading }</h1>
         </header>
         <ul class="cards-list"></ul>
         <footer>
-            <section class="input-section invisible">
-                <textarea 
+            <form class="input-section invisible">
+                <label for=${id} class="sr-only"> <!--Доступность-->
+                    Введите название карточки
+                </label>
+                <textarea
+                    id=${id} 
                     class="input-card" 
                     name="card-content" 
                     placeholder="Введите название карточки"
+                    maxlength="${ MAXLEN_CARD_INPUT }"
                 ></textarea>
                 <section class="controls">
                     <button class="confirm-add-card">Добавить карточку</button>
                     <span class="discard-add-card"></span>
                 </section>
-            </section>
+            </form>
             <button class="add-card">Добавить еще одну карточку</button>
         </footer>
     `
@@ -134,6 +186,15 @@ function generateColumn(clicked) {
 }
 
 
+function generateId(prefix) {
+    let id, check;
 
+    do {
+        const min = 10000;
+        const max = 99999;
+        id = prefix + Math.floor(Math.random() * (max - min) + min);
+        check = document.querySelector("#" + id);
+    } while(check)
 
-
+    return id;
+}
